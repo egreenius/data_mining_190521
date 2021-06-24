3d93a85fc16d9ea9784f3dfc0b92a43319fe0a68
Eugene Pashkevich <ep@ftclub.biz>
2021-06-24 23:59:20 +0300

    Homework for lesson 7

---
+++ main.py
@@ -0,0 +1,24 @@
+#pip install python-dotenv
+import os
+import dotenv
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+
+# from gb_parse.spiders.youla import AutoyoulaSpider
+# from gb_parse.spiders.hh import HhRemoteSpider
+# from gb_parse.spiders.avito import AvitoRealEstateSpider
+from inst_parse.spiders.instagram import InstagramSpider
+
+
+if __name__ == '__main__':
+    dotenv.load_dotenv(".env")
+    crawler_settings = Settings()
+    crawler_settings.setmodule("inst_parse.settings")
+    crawler_process = CrawlerProcess(settings=crawler_settings)
+    crawler_process.crawl(
+        InstagramSpider,
+        login=os.getenv('LOGIN'),
+        password=os.getenv('PASSWORD'),
+        tags= ["python", "java"],
+    )
+    crawler_process.start()
