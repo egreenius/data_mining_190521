eff42f940f1731faf07f192fc93ea90d60f0c008
Eugene Pashkevich <ep@ftclub.biz>
2021-06-25 00:06:26 +0300

    minor correction

--- inst_parse/items.py
+++ inst_parse/items.py
@@ -7,10 +7,6 @@ from scrapy.item import Item, Field
 
 
 class InstParseItem(Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    # date_parse = Field()
-    # data = Field()
     pass
 
 
